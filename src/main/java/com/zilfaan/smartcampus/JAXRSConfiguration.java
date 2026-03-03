a0444ef9ec6a84ae5e4c1abc63312dcfa640c89b
Zilfaan Zaki <zilfaanzaki@gmail.com>
2026-03-03 14:24:24 +0530

    Initial commit

---
+++ src/main/java/com/zilfaan/smartcampus/JAXRSConfiguration.java
@@ -0,0 +1,8 @@
+package com.zilfaan.smartcampus;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api/v1")
+public class JAXRSConfiguration extends Application {
+}
